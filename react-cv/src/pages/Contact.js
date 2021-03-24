5b47e84e1793560cdf7fd7eec45f3cfc0644c314
celia77940 <celiarameau@live.fr>
2021-03-24 20:41:47 +0100

    on avance

---
+++ react-cv/src/pages/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const Contacts = () => {
+    return (
+        <div>
+            <Navigation></Navigation>
+        </div>
+    );
+};
+
+export default Contacts;
---
+++ react-cv/src/pages/Knowledges.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const Knowledges = () => {
+    return (
+        <div>
+            <Navigation></Navigation>
+        </div>
+    );
+};
+
+export default Knowledges;
