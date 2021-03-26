82892429639e07b06bbf407104408489fb363bbe
celia77940 <celiarameau@live.fr>
2021-03-26 11:27:25 +0100

    fini compétences

--- react-cv/src/components/portfolio/ProjectList.js
+++ react-cv/src/components/portfolio/ProjectList.js
@@ -1,13 +1,68 @@
 import React, { Component } from 'react';
+import { portfolioData } from '../../data/portofolioData';
+import Project from './Projet';
 
 
 class ProjectList extends Component {
+    state = {
+        projects: portfolioData,
+        radios: [
+            {id: 1, value: "Html"},
+            {id: 2, value: "Css"},
+            {id: 3, value: "Php"},
+            {id: 4, value: "Javascript"},
+            {id: 5, value: "React"}
+        ], 
+        selectedRadio: 'Html'
+    };
+
+    handleRadio = (event) =>{
+        let radio = event.target.value;
+        this.setState({selectedRadio: radio})
+    }
+
     render() {
-        state
+        let { projects, radios, selectedRadio } = this.state 
+            
         return (
-            <div>
-                
-                
+            <div className="portfolioContent">
+                <ul className="radioDisplay">
+                    {
+                        radios.map((radio)=> {
+                            return(
+                            <li key={radio.id}>
+                                <input
+                                type="radio"
+                                name="radio"
+                                checked={radio.value === selectedRadio}
+                                value={radio.value}
+                                id={radio.value}
+                                onChange={this.handleRadio}
+                                />
+                                <label htmlFor={radio.value}>{radio.value}</label>
+                            </li>
+                            )
+                        })
+                    }
+
+                </ul>
+                <div className="projects">
+                    {
+                        projects
+                            // .filter(items => items.languages.includes(selectedRadio))
+                            .map(item => {
+                                return(
+                                    <Project
+                                    keys={item.id}
+                                    item={item}
+                                    />
+                                )
+                        })
+                    }
+
+                </div>
+
+                     
             </div>
         );
     }
