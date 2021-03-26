ffcdc691a0a0236e4da7e72938628dce61797367
celia77940 <celiarameau@live.fr>
2021-03-26 09:16:42 +0100

    portfolio

---
+++ react-cv/src/components/portfolio/ProjectList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class ProjectList extends Component {
+    render() {
+        state
+        return (
+            <div>
+                
+                
+            </div>
+        );
+    }
+}
+
+export default ProjectList;
---
+++ react-cv/src/data/portofolioData.js
@@ -0,0 +1,21 @@
+export const portfolioData = [
+    {
+      id: 1,
+      name: 'ipsum.com',
+      languages: ['javascript','css'],
+      languagesIcons: ['fab fa-js','fab fa-css3-alt'],
+      source: 'http://www.github.com',
+      info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
+      picture: './media/dashbord.jpg'
+    },
+    {
+      id: 2,
+      name: 'lorem.fr',
+      languages: ['javascript','react'],
+      languagesIcons: ['fab fa-js','fab fa-react'],
+      source: 'http://www.github.com',
+      info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
+      picture: './media/maquettesite.jpg'
+    },
+
+  ]
